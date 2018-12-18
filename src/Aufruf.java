ea1d1df8ff9f14450e742d7c90a4c30410204b6e
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-12-18 22:17:32 +0100

    Aufgabe4

--- src/Aufruf.java
+++ src/Aufruf.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Aufruf
 {
     public static void main (String[]args) // main Methode
@@ -12,5 +14,27 @@ public class Aufruf
         Student c = new Student("Nina", "Pfister", 21, 1810653169, "Web Business & Technology");
         c.Status(); // Methode Status, mit c. greife ich auf Student c zu
         // gleiche Methode in zwei verschiedenen Kontexten, Polymorphismus
+
+
+        //Aufgabe 4
+        String d = JOptionPane.showInputDialog("Gib etwas ein!"); // 3 Variablen vom Typ String mit Inhalten befüllen, InputDialog
+        String e = JOptionPane.showInputDialog("Gib etwas ein!");
+        String f = JOptionPane.showInputDialog("Gib etwas ein!");
+
+        if (d.equals(e)) // mit Methode equals werden Stings verglichen, == vergleicht Referenzen (Verweis auf Objekt) keine Werte, if weil man vergleichen will, wenn, wenn dann, wenn dann, dann
+        {
+            System.out.println("Zwei Variablen wurden gleich befüllt!"); //soll ausgeben das zwei Variablen gleich befüllt wurden
+        }
+        else if (d.equals(f)) // anderer Vergleich
+        {
+            System.out.println("Zwei Variablen wurden gleich befüllt!");
+        }
+        else if (e.equals(f)) // anderer Vergleich
+        {
+            System.out.println("Zwei Variablen wurden gleich befüllt!");
+        }
+        else {
+            System.out.println("Keine der Variablen ist gleich!"); // wenn keine der Variablen gleich befüllt wurde, dann soll es das ausgeben
+        }
     }
 }
